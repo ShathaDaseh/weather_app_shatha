0d737ab0067faa66499e3895d74b67c2a92e6ffd
Shatha Daseh <shathadash21@gmail.com>
2025-12-02 22:18:36 +0200

    Remove folder from repo only

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Weather App",
+      title: "Weather App new",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
